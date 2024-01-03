48ceae6ba7eeef66f069eb36ceb129ad9e62968a
billybautista <billy.bautista.cornelio@gmail.com>
2024-01-03 16:56:46 -0300

    add screens transition

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DrawerNavigation from './navigation/DrawerNavigation';
 import ArtworkDetail from './screens/DetailScreen';
 import Provider from './hooks/context';
 import SearchScreen from './screens/SearchScreen';
 import { AppRoute } from './navigation/AppRoute';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -18,9 +18,27 @@ export default function App() {
       <Provider>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name={AppRoute.DRAWER} component={DrawerNavigation} />
-            <Stack.Screen name={AppRoute.ARTWORK_DETAIL} component={ArtworkDetail} />
-            <Stack.Screen name={AppRoute.SEARCH} component={SearchScreen} />
+            <Stack.Screen
+              name={AppRoute.DRAWER}
+              component={DrawerNavigation}
+              options={{
+                animation: 'slide_from_left',
+              }}
+            />
+            <Stack.Screen
+              name={AppRoute.ARTWORK_DETAIL}
+              options={{
+                animation: 'slide_from_right',
+              }}
+              component={ArtworkDetail}
+            />
+            <Stack.Screen
+              name={AppRoute.SEARCH}
+              component={SearchScreen}
+              options={{
+                animation: 'slide_from_left',
+              }}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
